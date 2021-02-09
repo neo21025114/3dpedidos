96964ac35cf557b72cb5ae6e157621c0cf0fa3fb
Danrley Perez Sena <danrley2109@gmail.com>
2021-02-09 17:03:31 -0300

    retornando checkbox

--- 3Dpedidos.py
+++ 3Dpedidos.py
@@ -1,9 +1,14 @@
-
-
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from conexao import conexao1
+from kivy.uix.screenmanager import Screen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+from kivy.config import Config
 
+Config.set('graphics', 'resizable', False) # janela nao alteravel de tamanho
+#Config.set('graphics','width','500')  # largura janela
+#Config.set('graphics','height','500') #altura janela
 conn = conexao1()
 sqlittle = """SELECT * FROM Usuarios """
 
@@ -21,34 +26,48 @@ for i,j in usuario1:
     lista.append(i)
 
 
-class caixa(BoxLayout):
+class caixa(BoxLayout, Screen):
 
     def __init__(self, **kwargs):
         super().__init__(*kwargs)
 
         for element in lista:
             strelement = str(element)
-            self.ids.box3.add_widget(adc(text=strelement))
+            elementox = adc(text=strelement)
+            self.ids.box3.add_widget(elementox)
 
-   # def adda_widget(self):
-        #self.ids.box2.add_widget(adc1(text=strusuario))
+
+    def adda_widget(self):
+        if adc().check:
+            elementoy = adc().check()
+            print(elementoy)
+
+
+
 
    # def adda_widget1(self):
     #    self.ids.box3.add_widget(Butao2(text='adicionou'))
 
 
-
-
 class adc(BoxLayout):
     def __init__(self, text='', **kwargs):
         super(adc, self).__init__(*kwargs)
-        self.ids.b.text = text
+        self.ids.elemento.text = text
+        self.ids.q.text = text
+
+    def check(self):
+            elem_check = str(self.ids.elemento.text)
+            print(elem_check)
+            return elem_check
+
+
+
 
 
 class adc1(BoxLayout):
     def __init__(self, text='', **kwargs):
-        super(adc, self).__init__(*kwargs)
-        self.ids.c.text = text
+        super(adc1, self).__init__(*kwargs)
+        self.ids.q.text = text
 
 
 class maker_3d(App):
